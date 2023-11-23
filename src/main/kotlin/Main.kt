b50c391e76f008fb5f809fd11a14e7e612d44e80
Sasik-popishak <151189640+Sasik-popishak@users.noreply.github.com>
2023-11-23 15:10:42 +0300

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>)
+{
+    //1
+    val myage = 19
+    val IsTeenager = 13 < myage && myage < 19
+    //2
+    val  theirAge = 30
+    val isTeenager = ((13 < myage && myage < 19 )&&(13 < theirAge && theirAge< 19))
+    //3
+    val reader = "Stas"
+    val author = "Richard Lucas"
+    val auhorReder = reader < author
+    //4
+    val readerBeforeAuthor = ""
+    //5
+    val myAge2 = 19
+    if (13 < myage && myage < 19)
+        println("Подросток")
+    else
+        println("НЕ подросток")
+    //7
+}
